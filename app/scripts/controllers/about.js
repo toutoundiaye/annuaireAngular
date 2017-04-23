298f24f6ce92bbe2b4121ad0d760ee0bffcf7059
Toutou Baila Ndiaye <bailatoutou.ndiaye@gmail.com>
2017-04-23 11:11:30 +0200

    initial commit starting project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name annuaireAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the annuaireAngularApp
+ */
+angular.module('annuaireAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name annuaireAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the annuaireAngularApp
+ */
+angular.module('annuaireAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
